377f0f3ba0a4c00e5130e8fd5e510e184bb01fb9
martinsievers <sievers@uni-trier.de>
2012-09-05 16:23:05 +0200

    Modified the structure to match gadget approach. Made index.html independent of Trier changes by just importing the js files

--- js/wce_editor.js
+++ js/wce_editor.js
@@ -1,4 +1,6 @@
 function setWceEditor(_id) {
+	tinyMCE.baseURL= URI('js/').absoluteTo(gadgets.util.getUrlParameters()['url']);
+	tinyMCE.baseURI = new tinyMCE.util.URI(tinyMCE.baseURL);
 	tinyMCE.init({
 		// General options
 		mode : "exact", 
@@ -27,8 +29,11 @@ function setWceEditor(_id) {
 		theme_advanced_toolbar_location : "top",
 		theme_advanced_toolbar_align : "left",
 		theme_advanced_statusbar_location : "bottom",
-		theme_advanced_resizing : true
+		theme_advanced_resizing : false
 	});
+	//oninit : function() {  // Once initialized, tell the editor to go fullscreen
+	//	resizeHeight(115);
+	//}
 }
 
 var user_id, user_text_name;
@@ -68,32 +73,56 @@ function getXmlData() {
 	window.open('php/get_xml.php?userid=' + user_id + '&textname=' + user_text_name, 'xml');
 }
 
-// save editor-content to mysql-database
-function saveDataToDB(user_text_name, chapter, user_id) {
+
+function saveDataToDB() {
 	if (!tinyMCE.activeEditor.isDirty())
 		return;
 
-	for ( var i = 0; i < arguments.length; i++) {
-		if (typeof arguments[i] == 'undefined' || arguments[i] == '')
-			return;
-	}
+	// currently we grab the HTML span formatted data, but eventually we'd like to grab the TEI
+	var transcriptionData = getData();
 
-	$.ajax({
-		type : 'POST',
-		url : 'php/save.php',
-		data : {
-			'filename' : user_text_name,
-			'chapter' : chapter,
-			'text' : getData(),
-			'userid' : user_id,
-			'metadata' : getMetaData()
-		},
-		success : function(msg) {
-			setEditorNotDirty(1);
-			alert(msg + "Changes are saved ");
+
+	// currently we store to the portal user's personal data store, but eventually we'd like to
+	// store to the transcription repository
+	var req = opensocial.newDataRequest();
+	req.add(req.newUpdatePersonAppDataRequest("VIEWER", 'trans-'+lastPage.docid+'-'+lastPage.pageid, encodeURIComponent(transcriptionData)));
+	req.send(function(data) {
+		if (data.hadError()) {
+			alert(data.getErrorMessage());
+			return;
 		}
+		alert("Changes are saved.");
+		if (gadgets.util.hasFeature('pubsub-2')) gadgets.Hub.publish("interedition.transcription.saved", null);
 	});
 }
+
+// save editor-content to mysql-database
+
+// function saveDataToDB(user_text_name, chapter, user_id) {
+	// if (!tinyMCE.activeEditor.isDirty())
+		// return;
+
+	// for ( var i = 0; i < arguments.length; i++) {
+		// if (typeof arguments[i] == 'undefined' || arguments[i] == '')
+			// return;
+	// }
+
+	// $.ajax({
+		// type : 'POST',
+		// url : 'php/save.php',
+		// data : {
+			// 'filename' : user_text_name,
+			// 'chapter' : chapter,
+			// 'text' : getData(),
+			// 'userid' : user_id,
+			// 'metadata' : getMetaData()
+		// },
+		// success : function(msg) {
+			// setEditorNotDirty(1);
+			// alert(msg + "Changes are saved ");
+		// }
+	// });
+// }
 
 //
 function setMetaData(msg) {
