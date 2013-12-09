cc860e5e6c9ebb5ce1bdb3b7a709be43dcf386f4
yugan <gany2d01@uni-trier.de>
2013-12-09 16:52:28 +0100

    Fixed  #1766: seg's around punctuation are being lost in uploaded files.

--- js/wce_tei.js
+++ js/wce_tei.js
@@ -2,7 +2,7 @@
 window.onerror = Fehlerbehandlung;
 
 //pb, cb ,lb with break="no" defined in function html2Tei_mergeWNode();
-var wceNodeInsideW=["hi","unclear","gap","supplied", "w", "abbr", "pc", "ex"];//TODO: more type?
+var wceNodeInsideW=["hi","unclear","gap","supplied", "w", "abbr", "ex"];//TODO: more type?
 
 function Fehlerbehandlung(Nachricht, Datei, Zeile) {
 	Fehler = "Error:\n" + Nachricht + "\n" + Datei + "\n" + Zeile;
@@ -3401,9 +3401,20 @@ function getTeiByHtml(inputString, args) {
 	 * type pc, return <pc>
 	 */
 	var html2Tei_pc = function(arr, $teiParent, $htmlNode) {
+		//Fixed #1766
+		var pre=$htmlNode.previousSibling;
+		var next=$htmlNode.nextSibing;
+		if(pre && pre.nodeName=='w'){
+			pre.setAttribute('after','1');
+		}
+		if(next && next.nodeName=='w'){
+			next.setAttribute('before','1');
+		}
+		
 		var pc = $newDoc.createElement('pc');
-		//$teiParent.appendChild(pc);
-		appendNodeInW($teiParent, pc, $htmlNode);
+		nodeAddText(pc,getDomNodeText($htmlNode));
+		$teiParent.appendChild(pc);
+		//appendNodeInW($teiParent, pc, $htmlNode);
 		return {
 			0 : pc,
 			1 : true
