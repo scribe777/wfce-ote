2f3faa8983c93ff0953a375e765a6bebdef73bd8
martinsievers <sievers@uni-trier.de>
2015-02-17 11:13:25 +0100

    Version v1.3.5

--- js/plugins/wce/editor_plugin_src.js
+++ js/plugins/wce/editor_plugin_src.js
@@ -33,7 +33,7 @@
 */
 
 (function() {
-	var wfce_editor = "1.3.5BETA (2015-02-17)";
+	var wfce_editor = "1.3.5 (2015-02-17)";
 
 	// Load plugin specific language pack
 	tinymce.PluginManager.requireLangPack('wce');
