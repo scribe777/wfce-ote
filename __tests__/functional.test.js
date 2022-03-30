0709557f25947de5e1d04ce3c87ca0b4e476646a
cat <910384+catsmith@users.noreply.github.com>
2022-03-30 19:51:54 +0100

    test improved functional tests

--- __tests__/functional.test.js
+++ __tests__/functional.test.js
@@ -9,12 +9,46 @@ test('test index page', async () => {
   });
   const page = await browser.newPage();
   await page.goto(`file:${path.join(__dirname, '..', 'wce-ote', 'index.html')}`);
-
-
   const frameHandle = await page.$("iframe[id='wce_editor_ifr']");
   const frame = await frameHandle.contentFrame();
   await frame.type('body#tinymce', 'my words');
   const htmlData = await page.evaluate(`getData()`);
   expect(htmlData).toBe('my words');
+  await browser.close();
+}, 10000);
+
+
+test('test highlight text', async () => {
+  const browser = await puppeteer.launch({
+    headless: true,
+    // slowMo: 80,
+    // args: ['--window-size=1920,1080', '--disable-web-security']
+    args: ['--disable-web-security']
+  });
+  const page = await browser.newPage();
+  await page.goto(`file:${path.join(__dirname, '..', 'wce-ote', 'index.html')}`);
+
+
+  const frameHandle = await page.$('iframe[id="wce_editor_ifr"]');
+  const frame = await frameHandle.contentFrame();
+  await frame.type('body#tinymce', 'my words');
+  await page.keyboard.down('Shift');
+  for (let i = 0; i < 'rds'.length; i++) {
+    await page.keyboard.press('ArrowLeft');
+  };
+  await page.keyboard.up('Shift');
+  await page.click('button#mceu_12-open');
+  await page.keyboard.press('ArrowDown');
+  await page.keyboard.press('ArrowRight');
+  await page.keyboard.press('Enter');
+  const menuFrameHandle = await page.$('div[id="mceu_40"] > div > div > iframe');
+  const menuFrame = await menuFrameHandle.contentFrame();
+  await menuFrame.select('select[id="unclear_text_reason"]', 'damage to page');
+  await menuFrame.click('input#insert');
+  const htmlData = await page.evaluate(`getData()`);
+  expect(htmlData).toBe('my wo<span class="unclear" wce_orig="rds" wce="__t=unclear&amp;__n=&amp;help=Help' +
+                        '&amp;unclear_text_reason=damage%20to%20page&amp;unclear_text_reason_other=">' +
+                        '<span class="format_start mceNonEditable">‹</span>ṛḍṣ' +
+                        '<span class="format_end mceNonEditable">›</span></span>');
   await browser.close();
 }, 10000);
