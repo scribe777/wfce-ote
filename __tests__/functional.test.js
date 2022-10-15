a10d751de26ade954c08abb709c1589b8ffa9f44
Cat Smith <910384+catsmith@users.noreply.github.com>
2022-10-15 23:13:10 +0100

    code and tests written

--- __tests__/functional.test.js
+++ __tests__/functional.test.js
@@ -186,7 +186,10 @@ describe('testing with default client settings', () => {
     // access menu window and make selection
     const menuFrameHandle = await page.$('div[id="mceu_41"] > div > div > iframe');
     const menuFrame = await menuFrameHandle.contentFrame();
+    // check extent is not populated (since we have a setting for that now)
+    expect(await menuFrame.$eval('#sp_extent', el => el.value)).toBe('');
     await menuFrame.type('input#sp_extent', '5');
+    //NB the selected input will be char as that is at the top of the list and there is no empty select option
     await menuFrame.click('input#insert');
     await page.waitForSelector('div[id="mceu_41"]', {hidden: true});
     const htmlData = await page.evaluate(`getData()`);
@@ -2152,5 +2155,133 @@ describe('testing with different client settings', () => {
 
 
   // Ending here the functional tests to test the new option to provide a list of books a select in the V menu
+
+});
+
+describe('testing with defaultValuesForSpaceMenu setting', () => {
+
+  beforeEach(async () => {
+    let frameHandle;
+    jest.setTimeout(5000000);
+    page = await browser.newPage();
+    await page.goto(`file:${path.join(__dirname, 'test_index_page.html')}`);
+    // because char is the first option in the select it is always populated with that so I'm using line instead
+    await page.evaluate(`setWceEditor('wce_editor', {defaultValuesForSpaceMenu: {'unit': 'line', 'extent': 5}})`);
+    page.waitForSelector("#wce_editor_ifr");
+    frameHandle = null;
+    while (frameHandle === null) {
+      frameHandle = await page.$("iframe[id='wce_editor_ifr']");
+    }
+    frame = await frameHandle.contentFrame();
+
+  });
+
+  test('test default space options', async () => {
+    await frame.type('body#tinymce', 'space between  words');
+    for (let i = 0; i < ' words'.length; i++) {
+      await page.keyboard.press('ArrowLeft');
+    }
+    // open P menu
+    await page.click('button#mceu_17-open');
+    // navigate submenu
+    await page.keyboard.press('ArrowDown');
+    await page.keyboard.press('ArrowDown');
+    await page.keyboard.press('ArrowRight');
+    await page.keyboard.press('Enter');
+    // access menu window and make selection
+    const menuFrameHandle = await page.$('div[id="mceu_41"] > div > div > iframe');
+    const menuFrame = await menuFrameHandle.contentFrame();
+    // check extent is pre-populated 
+    expect(await menuFrame.$eval('#sp_extent', el => el.value)).toBe('5');
+    //check unit is pre-populated
+    expect(await menuFrame.$eval('#sp_unit', el => el.value)).toBe('line');
+    await menuFrame.click('input#insert');
+    await page.waitForSelector('div[id="mceu_41"]', {hidden: true});
+    const htmlData = await page.evaluate(`getData()`);
+    expect(htmlData).toBe('space between <span class=\"spaces\" wce=\"__t=spaces&amp;__n=&amp;original_spaces_text=&amp;help=Help&amp;sp_unit=line&amp;sp_unit_other=&amp;sp_extent=5\"><span class=\"format_start mceNonEditable\">‹</span>sp<span class=\"format_end mceNonEditable\">›</span></span> words');
+    const xmlData = await page.evaluate(`getTEI()`);
+    expect(xmlData).toBe(xmlHead + '<w>space</w><w>between</w><space unit="line" extent="5"/><w>words</w>' + xmlTail);
+  }, 200000);
+
+  test('test default space options can be overriden', async () => {
+    await frame.type('body#tinymce', 'space between  words');
+    for (let i = 0; i < ' words'.length; i++) {
+      await page.keyboard.press('ArrowLeft');
+    }
+    // open P menu
+    await page.click('button#mceu_17-open');
+    // navigate submenu
+    await page.keyboard.press('ArrowDown');
+    await page.keyboard.press('ArrowDown');
+    await page.keyboard.press('ArrowRight');
+    await page.keyboard.press('Enter');
+    // access menu window and make selection
+    const menuFrameHandle = await page.$('div[id="mceu_41"] > div > div > iframe');
+    const menuFrame = await menuFrameHandle.contentFrame();
+    // check extent is not populated (since we have a setting for that now)
+    expect(await menuFrame.$eval('#sp_extent', el => el.value)).toBe('5');
+    await menuFrame.click('input#sp_extent');
+    await page.keyboard.press('Backspace');
+    await menuFrame.type('input#sp_extent', '4');
+    //check unit is pre-populated
+    expect(await menuFrame.$eval('#sp_unit', el => el.value)).toBe('line');
+    await menuFrame.select('select[id="sp_unit"]', 'char');
+    await menuFrame.click('input#insert');
+    await page.waitForSelector('div[id="mceu_41"]', {hidden: true});
+    const htmlData = await page.evaluate(`getData()`);
+    expect(htmlData).toBe('space between <span class=\"spaces\" wce=\"__t=spaces&amp;__n=&amp;original_spaces_text=&amp;help=Help&amp;sp_unit=char&amp;sp_unit_other=&amp;sp_extent=4\"><span class=\"format_start mceNonEditable\">‹</span>sp<span class=\"format_end mceNonEditable\">›</span></span> words');
+    const xmlData = await page.evaluate(`getTEI()`);
+    expect(xmlData).toBe(xmlHead + '<w>space</w><w>between</w><space unit="char" extent="4"/><w>words</w>' + xmlTail);
+  }, 200000);
+
+});
+
+describe('testing with invalid defaultValuesForSpaceMenu settings', () => {
+
+  beforeEach(async () => {
+    let frameHandle;
+    jest.setTimeout(5000000);
+    page = await browser.newPage();
+    await page.goto(`file:${path.join(__dirname, 'test_index_page.html')}`);
+    // because char is the first option in the select it is always populated with that so I'm using line instead
+    await page.evaluate(`setWceEditor('wce_editor', {defaultValuesForSpaceMenu: {'unit': 'nonsense', 'extent': 'five'}})`);
+    page.waitForSelector("#wce_editor_ifr");
+    frameHandle = null;
+    while (frameHandle === null) {
+      frameHandle = await page.$("iframe[id='wce_editor_ifr']");
+    }
+    frame = await frameHandle.contentFrame();
+
+  });
+
+  test('test incorrect default behaviour and that they can still be overridden by user actions', async () => {
+    await frame.type('body#tinymce', 'space between  words');
+    for (let i = 0; i < ' words'.length; i++) {
+      await page.keyboard.press('ArrowLeft');
+    }
+    // open P menu
+    await page.click('button#mceu_17-open');
+    // navigate submenu
+    await page.keyboard.press('ArrowDown');
+    await page.keyboard.press('ArrowDown');
+    await page.keyboard.press('ArrowRight');
+    await page.keyboard.press('Enter');
+    // access menu window and make selection
+    const menuFrameHandle = await page.$('div[id="mceu_41"] > div > div > iframe');
+    const menuFrame = await menuFrameHandle.contentFrame();
+    // check extent is not pre-populated because the value supplied was not a number 
+    expect(await menuFrame.$eval('#sp_extent', el => el.value)).toBe('');
+    await menuFrame.click('input#sp_extent');
+    await menuFrame.type('input#sp_extent', '5');
+    //check unit is not pre-populated because the requested value isn't in the option list (it will be empty even though there is no 'empty' option)
+    expect(await menuFrame.$eval('#sp_unit', el => el.value)).toBe('');
+    await menuFrame.select('select[id="sp_unit"]', 'line');
+    await menuFrame.click('input#insert');
+    await page.waitForSelector('div[id="mceu_41"]', {hidden: true});
+    const htmlData = await page.evaluate(`getData()`);
+    expect(htmlData).toBe('space between <span class=\"spaces\" wce=\"__t=spaces&amp;__n=&amp;original_spaces_text=&amp;help=Help&amp;sp_unit=line&amp;sp_unit_other=&amp;sp_extent=5\"><span class=\"format_start mceNonEditable\">‹</span>sp<span class=\"format_end mceNonEditable\">›</span></span> words');
+    const xmlData = await page.evaluate(`getTEI()`);
+    expect(xmlData).toBe(xmlHead + '<w>space</w><w>between</w><space unit="line" extent="5"/><w>words</w>' + xmlTail);
+  }, 200000);
 
 });
