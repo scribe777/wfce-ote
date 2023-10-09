f1bf3ee7a884531f5d099906ae375d222cdd0a3b
Cat Smith <910384+catsmith@users.noreply.github.com>
2023-10-09 12:03:26 +0100

    increment minor version

--- wce-ote/plugin/plugin.js
+++ wce-ote/plugin/plugin.js
@@ -33,7 +33,7 @@
 */
 
 (function() {
-	var wfce_editor = "3.0.0 (2022-11-05)";
+	var wfce_editor = "3.0.1 (2023-10-09)";
 
 	// Load plugin specific language pack
 	tinymce.PluginManager.requireLangPack('wce');
